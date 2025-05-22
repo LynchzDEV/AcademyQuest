ebb6b18490c3740f519701f1a61e468d8bab1cd2
LynchzDEV <opmok48@gmail.com>
2025-05-22 09:53:00 +0700

    add /brags into config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "quests#index"
   resources :quests
 
+  get "/brag", to: "brags#index", as: :brags
   get "up" => "rails/health#show", as: :rails_health_check
 end
